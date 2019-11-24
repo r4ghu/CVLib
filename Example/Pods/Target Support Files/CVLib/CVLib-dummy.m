ccd867f720509b56348e03a3937fb42560cc231a
r4ghu <m.sriraghu@gmail.com>
2019-11-23 22:34:19 -0800

    Initial commit with pod lib template

---
+++ Example/Pods/Target Support Files/CVLib/CVLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CVLib : NSObject
+@end
+@implementation PodsDummy_CVLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-CVLib_Example/Pods-CVLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CVLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CVLib_Example
+@end
